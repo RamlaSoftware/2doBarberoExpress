16bfd876a657485cb5a6962ba482eed3bab179f5
Cristian Franco <cfrancobedoya@gmail.com>
2017-12-07 11:53:42 -0500

    ARREGLOS TAMAÑO IMAGENES

--- js/productos_index.js
+++ js/productos_index.js
@@ -46,7 +46,7 @@ if (navigator.userAgent.indexOf("Chrome") != -1) {
       }
 
     }
-    Fotos_Index();
+    //Fotos_Index();
     Imegenes_Recomendadas();
   }, 2000);
 
@@ -64,7 +64,7 @@ if (navigator.userAgent.indexOf("Chrome") != -1) {
       }
 
     }
-    Fotos_Index();
+    //Fotos_Index();
     Imegenes_Recomendadas();
   }, 1000);
 
@@ -116,43 +116,180 @@ function Imegenes_Recomendadas() {
   //}, 3000);
 }
 
-// CARGA IMAGENES PORTAFOLIO
-function Fotos_Index() {
-  var imagenes_referidos = '';
+
+// CARGA IMAGENES POR TIPOS (PORTAFOLIO)
+function imagenesTipos(){
+  var imagenesTipo = '';
   var j = 2;
-  var No_productos = 0; // CANTIDAD DE PRODUCTOS A MOSTRAR
+  var noProductos = 5; // 5 - 2 = 3
   var completo = false;
-  var contador = 0;
-  while (j < recomendado.length && contador < 3) {
-    if (recomendado[j] == true) {
-      imagenes_referidos += '<div class="col-sm-4 mb-xs-30 wow fadeInUp" data-wow-delay="0.' + contador + 's">'
-                            + '<div class="team-item">'
-                              + '<div class="team-item-image">'
-                                + '<img src="' + foto_Url[j] + '" onclick="Ir_producto(' + "'" + keyProducto[j] + "'" + ')" alt=""/>'
-                                + '<div class="team-item-detail">'
-                                  + '<h4 class="font-alt normal">Nice to meet!</h4>'
-                                  + '<p> Curabitur augue, nec finibus mauris pretium eu. Duis placerat ex gravida nibh tristique porta.</p>'
-                                  + '<div class="team-social-links">'
-                                    + '<a href="#" target="_blank"><i class="fa fa-facebook"></i></a>'
-                                    + '<a href="#" target="_blank"><i class="fa fa-twitter"></i></a>'
-                                    + '<a href="#" target="_blank"><i class="fa fa-pinterest"></i></a>'
-                                  + '</div>'
-                                + '</div>'
-                              + '</div>'
-                              + '<div class="team-item-descr font-alt">'
-                                + '<div class="team-item-name">'
-                                  + nombre[j]
-                                + '</div>'
-                                + '<div class="team-item-role">'
-                                  + '$' + precio[j]
-                                + '</div>'
-                              + '</div>'
-                            + '</div>'
-                          + '</div>';
-      No_productos++;
-      contador++;
-    }
+  while(j < noProductos){
+    var aleatorio = Math.floor((Math.random() * 15) + 1);
+    imagenesTipo +='<li class="work-item mix general">'
+                    + '<a href="portfolio-single-1.html" class="work-ext-link">'
+                      + '<div class="work-img">'
+                        + '<img class="work-img" src="'+ foto_Url[aleatorio] + '" alt="Work" />'
+                      + '</div>'
+                      + '<div class="work-intro">'
+                        + '<h3 class="work-title">' + nombre[aleatorio] + '</h3>'
+                        + '<div class="work-descr">'
+                          + 'Barbero Express'
+                        + '</div>'
+                      + '</div>'
+                    + '</a>'
+                    + '<div class="">'
+                      + '<p class="text-center">' + nombre[aleatorio] + ' </br> $' + precio[aleatorio] + '</p>'
+                    + '</div>'
+                  + '</li>';
     j++;
   }
-  document.getElementById("imagenes_recomendados").innerHTML = imagenes_referidos;
+  document.getElementById("work-grid").innerHTML = imagenesTipo;
+}
+
+
+//CARGAR IMAGENES PORTAFOLIO RESUMEN
+function imagenesPortafolioGeneral(){
+  var imagenesPortafolio = '';
+  var j = 0;
+  var noProductos = 6;
+  var ubicacionImagen = ["images/portAfolio/barbera.jpg","images/portafolio/maquina.jpg","images/portafolio/tijeras.jpg","images/portafolio/brush.jpg","images/portafolio/crazyGel.jpg","images/portafolio/espuma.jpg"];
+  var nombreImagen = ["BARBERAS/CUCHILLAS","MÁQUINAS","TIJERAS","SACUDIDORES/OTROS","CERAS/GEL","TÓNICOS/ESPUMAS"];
+  var contador = 0;
+    imagenesPortafolio += '<div class="relative" id="ayudaJesus">'
+                        + '<h2 class="section-title font-alt mb-70 mb-sm-40">'
+                        + 'Productos'
+                        + '</h2>'
+                        + '<!-- Works Filter -->'
+                        + '<div class="works-filter font-alt align-center">'
+                        + '<!-- <a href="#" class="filter active" data-filter="*">Todos</a> -->'
+                        + '<!-- <a href="#branding" class="filter" data-filter=".branding">Máquinas</a>'
+                        + '<a href="#design" class="filter" data-filter=".design">Tijeras</a>'
+                        + '<a href="#photography" class="filter" data-filter=".photography">Photography</a> -->'
+                        + '<a href="#" class="filter active" data-filter="" onclick="imagenesPortafolioGeneral()">Resumen</a>'
+                        + '<a href="#barberasCuchillas" class="filter" data-filter=".barberasCuchillas" onclick="imagenesTipos()">Barberas/Cuchillas</a>'
+                        + '<a href="#maquinas" class="filter" data-filter=".maquinas" onclick="imagenesTipos()">Máquinas</a>'
+                        + '<a href="#tijeras" class="filter" data-filter=".tijeras">Tijeras</a>'
+                        + '<a href="#sacudidoresOtros" class="filter" data-filter=".sacudidoresOtros">Sacudidores/Otros</a>'
+                        + '<a href="#cerasGel" class="filter" data-filter=".cerasGel">Ceras/Gel</a>'
+                        + '<a href="#tonicosEspumas" class="filter" data-filter=".tonicosEspumas">Tónicos/Espumas</a>'
+                        + '</div>'
+                        + '<!-- End Works Filter -->'
+                        + '<!-- Works Grid -->'
+                        + '<div id="tipos">'
+                        + '<ul class="works-grid work-grid-3 work-grid-gut clearfix font-alt hover-white hide-titles" id="work-grid">'
+                        + '<!-- Work Item (External Page) -->'
+                        + '<li class="work-item mix general barberasCuchillas">'
+                        + '<a href="portfolio-single-1.html" class="work-ext-link">'
+                        + '<div class="work-img">'
+                        + '<img class="work-img" src="images/portAfolio/barbera.jpg" alt="Work" />'
+                        + '</div>'
+                        + '<div class="work-intro">'
+                        + '<h3 class="work-title">Barberas/Cuchillas</h3>'
+                        + '<div class="work-descr">'
+                        + 'La mejor calidad'
+                        + '</div>'
+                        + '</div>'
+                        + '</a>'
+                        + '<div class="">'
+                        + '<p class="text-center">BARBERAS/CUCHILLAS</p>'
+                        + '</div>'
+                        + '</li>'
+                        + '<!-- End Work Item -->'
+                        + '<!-- Work Item (Lightbox) -->'
+                        + '<li class="work-item mix general">'
+                        + '<a href="images/portafolio/maquina.jpg" class="work-lightbox-link mfp-image">'
+                        + '<div class="work-img">'
+                        + '<img src="images/portafolio/maquina.jpg" alt="Work" />'
+                        + '</div>'
+                        + '<div class="work-intro">'
+                        + '<h3 class="work-title">Máquinas</h3>'
+                        + '<div class="work-descr">'
+                        + 'Las mejores marcas'
+                        + '</div>'
+                        + '</div>'
+                        + '</a>'
+                        + '<div class="">'
+                        + '<p class="text-center">MÁQUINAS</p>'
+                        + '<!-- <p class="text-center">TÓNICO <br> $2.500</p> -->'
+                        + '</div>'
+                        + '</li>'
+                        + '<!-- End Work Item -->'
+                        + '<!-- Work Item (External Page) -->'
+                        + '<li class="work-item mix general">'
+                        + '<a href="portfolio-single-1.html" class="work-ext-link">'
+                        + '<div class="work-img">'
+                        + '<img class="work-img" src="images/portafolio/tijeras.jpg" alt="Work" />'
+                        + '</div>'
+                        + '<div class="work-intro">'
+                        + '<h3 class="work-title">Tijeras</h3>'
+                        + '<div class="work-descr">'
+                        + 'Diseños especiales'
+                        + '</div>'
+                        + '</div>'
+                        + '</a>'
+                        + '<div class="">'
+                        + '<p class="text-center">TIJERAS</p>'
+                        + '</div>'
+                        + '</li>'
+                        + '<!-- End Work Item -->'
+                        + '<!-- Work Item (External Page) -->'
+                        + '<li class="work-item mix general">'
+                        + '<a href="portfolio-single-1.html" class="work-ext-link">'
+                        + '<div class="work-img">'
+                        + '<img class="work-img" src="images/portafolio/brush.jpg" alt="Work" />'
+                        + '</div>'
+                        + '<div class="work-intro">'
+                        + '<h3 class="work-title">Sacudidor/Otros</h3>'
+                        + '<div class="work-descr">'
+                        + 'El mejor complemento para tu trabajo.'
+                        + '</div>'
+                        + '</div>'
+                        + '</a>'
+                        + '<div class="">'
+                        + '<p class="text-center">SACUDIDORES/OTROS</p>'
+                        + '</div>'
+                        + '</li>'
+                        + '<!-- End Work Item -->'
+                        + '<!-- Work Item (External Page) -->'
+                        + '<li class="work-item mix general">'
+                        + '<a href="portfolio-single-1.html" class="work-ext-link">'
+                        + '<div class="work-img">'
+                        + '<img class="work-img" src="images/portafolio/crazyGel.jpg" alt="Work" />'
+                        + '</div>'
+                        + '<div class="work-intro">'
+                        + '<h3 class="work-title">Ceras/Gel</h3>'
+                        + '<div class="work-descr">'
+                        + 'Excelentes fragancias y calidad.'
+                        + '</div>'
+                        + '</div>'
+                        + '</a>'
+                        + '<div class="">'
+                        + '<p class="text-center">CERAS/GEL</p>'
+                        + '</div>'
+                        + '</li>'
+                        + '<!-- End Work Item -->'
+                        + '<!-- Work Item (External Page) -->'
+                        + '<li class="work-item mix general">'
+                        + '<a href="portfolio-single-1.html" class="work-ext-link">'
+                        + '<div class="work-img">'
+                        + '<img class="work-img" src="images/portafolio/espuma.jpg" alt="Work" />'
+                        + '</div>'
+                        + '<div class="work-intro">'
+                        + '<h3 class="work-title">Tónicos/Espumas</h3>'
+                        + '<div class="work-descr">'
+                        + 'Las mejores marcas de América'
+                        + '</div>'
+                        + '</div>'
+                        + '</a>'
+                        + '<div class="">'
+                        + '<p class="text-center">TÓNICOS/ESPUMAS</p>'
+                        + '</div>'
+                        + '</li>'
+                        + '<!-- End Work Item -->'
+                        + '</ul>'
+                        + '<!-- End Works Grid -->'
+                        + '</div>'
+                        + '</div>';
+
+  document.getElementById("portfolio").innerHTML = imagenesPortafolio;
 }
